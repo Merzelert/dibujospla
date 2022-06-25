d7f8b2b2f90b9a17d1a98c73d6b529a1b3ad1754
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-06-24 19:46:58 -0500

    imagenes

--- villa.js
+++ villa.js
@@ -2,12 +2,36 @@ var vm = document.getElementById("villamerz");
 var papel = vm.getContext("2d");
 var mapa = "tile.webp"
 
-var imagen = new Image();
-imagen.src = mapa; 
-imagen.addEventListener("load", dibujar);
+var fondo = new Image();
+fondo.src = mapa; 
+fondo.addEventListener("load", dibujar);
+
+var vaca = new Image();
+vaca.src = "vaca.webp";
+vaca.addEventListener("load", dibujarVaca);
+
+var pollo = new Image();
+pollo.src = "pollo.webp";
+pollo.addEventListener("load", dibujarPollo);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.webp";
+cerdo.addEventListener("load", dibujarCerdo);
 
 function dibujar() {
-    papel.drawImage(imagen, 0, 0);
+    papel.drawImage(fondo, 0, 0);
+}
+
+function dibujarVaca() {
+    papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarPollo() {
+    papel.drawImage(pollo, 50, 50);
+}
+
+function dibujarCerdo() {
+    papel.drawImage(cerdo, 100, 100);
 }
 
 function aleatorio(min, maxi) {
